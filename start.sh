b7de084b134281fd53ab0f6921e0e5ac5ea6b1f2
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-07-23 13:04:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clonehttps://github.com/NIHUU/Bot-V3.git /Bot-V3
+  git clone https://github.com/MOH-NIHAAL/bot-v3.git /bot-v3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Bot-V3
+  git clone $UPSTREAM_REPO /bot-v3
 fi
-cd /Bot-V3
+cd /bot-v3
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py

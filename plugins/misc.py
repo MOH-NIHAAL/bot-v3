a77ff7c943ad0915c509bf61f4712f6af3509622
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-07-23 14:00:21 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -4,6 +4,7 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Media
 from info import IMDB_TEMPLATE
 from utils import extract_user, get_file_id, get_poster, last_online, temp
 import time
+from Script import script
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import logging
